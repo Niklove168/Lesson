8b8365a7a7d4e7b5d96437a2f3d935afb9d3e303
Niklove168 <Nik210790@gmail.com>
2024-12-20 12:40:29 +0300

    Add files via upload

---
+++ Lesson5/PhoneBook.java
@@ -0,0 +1,21 @@
+package Lesson5;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PhoneBook {
+    private Map<String, List<String>> book;
+
+    public PhoneBook() {
+        book = new HashMap<>();
+    }
+
+    public void add(String surname, String number) {
+        book.computeIfAbsent(surname, k -> new ArrayList<>()).add(number);
+    }
+    public List<String>get(String surname){
+        return book.getOrDefault(surname,new ArrayList<>());
+    }
+}
